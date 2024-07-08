af2a8f5eeab522e3114378f36808686bdf9b9b7b
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-08 16:45:36 +0300

    feat: Add Step schema and reference User collection

---
+++ models/step.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+// id, name, description, createdByUserId
+const stepSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    description: {
+        type: String,
+        required: true,
+        minLength: 10
+    },
+    // reference Users collection --> 1:M relationship (1 user-to-Many steps)
+    user : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+})
+
+// transform DB fields in collection
+stepSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Step', stepSchema)
