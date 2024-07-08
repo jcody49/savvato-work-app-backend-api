019c0a972fbd9d1884bfc6f875670e88d7bc09ec
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-08 16:41:29 +0300

    chore: Manually add changes from feat/auth-cookie

--- controllers/login.js
+++ controllers/login.js
@@ -25,15 +25,24 @@ loginRouter.post('/', async(request, response) => {
         username: registeredUser.username,
         id: registeredUser.id
     }
-    // expires in 15 minutes
+    // expires in 60 minutes
     const token = jwt.sign(
         userToAuthenticate, 
         config.SECRET, {
-        expiresIn: 15*60    
+        expiresIn: 60 * 60    
     })
+    
+    // use httpOnly cookie to wrap JWT
+    response.cookie('auth_token', token, {
+        httpOnly: true,
+        secure: false, // true if https
+        maxAge: 60 * 60 * 1000, // match token expiresIn
+        sameSite: 'strict' // based on cross-site request needs
+    })
+
     response
-    .status(200)
-    .send({ token, username: registeredUser.username, name: registeredUser.name })
+        .status(200)
+        .send({ username: registeredUser.username, name: registeredUser.name })
 })
 
 
